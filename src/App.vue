395eb27ac87db0fcf7e735e256c3d0012e428199
AdonisVentocillaSanchez <aventocillas@autonoma.edu.pe>
2021-08-21 07:07:43 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav
+      class="navbar navbar-expand-lg navbar-light bg-light"
+      style="background-color: #e3f2fd;"
+    >
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">VacunaChain</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Registrar Vacunación</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Consultar Registro</a>
+            </li>
+            <li class="nav-item">
+              <router-link to="/registrar" class="nav-link" href="#">Registrar Vacunación</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/consultar" class="nav-link" href="#">Consultar Registro</router-link>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+
+    <RegisterVaccine title="Registra un nuevo paciente" />
+    <ListVaccine title="dg" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RegisterVaccine from "./components/RegisterVaccine.vue";
+import ListVaccine from "./components/ListVaccine.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    RegisterVaccine,
+    ListVaccine
   }
-}
+};
 </script>
 
 <style>
@@ -23,6 +72,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/RegisterVaccine.vue
@@ -0,0 +1,229 @@
+<template>
+  <div class="register">
+    <div id="formulario" class="container">
+      <h1>{{ title }}</h1>
+      <h2 class="mb-3">Paciente</h2>
+      <div class="container row">
+        <div class="mb-3 col-6">
+          <label for="tipodoc" class="form-label">Tipo Documento</label>
+          <select id="tipodoc" class="form-select" v-model="tipodoc">
+            <option selected>Selecione</option>
+            <option value="DNI">D.N.I</option>
+            <option value="Carnet">Carnet de Extranjería</option>
+          </select>
+        </div>
+        <div class="mb-3 col-6">
+          <label for="nrodoc" class="form-label">Nro Documento</label>
+          <input
+            type="number"
+            class="form-control"
+            id="nrodoc"
+            v-model="RegistroVacunas.NumeroDocumento"
+          />
+        </div>
+        <div class="mb-3 col-6 col-md-4">
+          <label for="firstname" class="form-label">Nombres</label>
+          <input
+            type="text"
+            class="form-control"
+            id="firstname"
+            v-model="RegistroVacunas.Nombres"
+          />
+        </div>
+        <div class="mb-3 col-6 col-md-4">
+          <label for="lastname1" class="form-label">Apellido Paterno</label>
+          <input
+            type="text"
+            class="form-control"
+            id="lastname1"
+            v-model="RegistroVacunas.ApellidoPaterno"
+          />
+        </div>
+        <div class="mb-3 col-6 col-md-4">
+          <label for="lastname2" class="form-label">Apellido Materno</label>
+          <input
+            type="text"
+            class="form-control"
+            id="lastname2"
+            v-model="RegistroVacunas.ApellidoMaterno"
+          />
+        </div>
+        <div class="mb-3 col-6 col-md-3">
+          <label for="phone" class="form-label">Teléfono</label>
+          <input
+            type="number"
+            class="form-control"
+            id="phone"
+            v-model="RegistroVacunas.Telefono"
+          />
+        </div>
+        <div class="mb-3 col-6 col-md-3">
+          <label for="VaccineBrand" class="form-label"
+            >Marca de la Vacuna</label
+          >
+          <select
+            id="VaccineBrand"
+            class="form-select"
+            v-model="RegistroVacunas.Dosis.MarcaVacuna"
+          >
+            <option selected>Selecione</option>
+            <option value="Pfizer">Pfizer</option>
+            <option value="AstraZeneca">AstraZeneca</option>
+            <option value="Sinopharm">Sinopharm</option>
+          </select>
+        </div>
+        <div class="mb-3 col-6 col-md-3">
+          <label for="VaccinationSite" class="form-label"
+            >Lugar de vacunación</label
+          >
+          <input
+            type="text"
+            class="form-control"
+            id="VaccinationSite"
+            v-model="RegistroVacunas.Dosis.LugarVacunacion"
+          />
+        </div>
+        <div class="mb-3 col-6 col-md-3">
+          <label for="datetime" class="form-label">Fecha de vacunación</label>
+          <input
+            type="date"
+            class="form-control"
+            id="datetime"
+            v-model="RegistroVacunas.Dosis.FechaVacunacion"
+          />
+        </div>
+      </div>
+      <h2 class="mb-3">Doctora</h2>
+      <div class="container row">
+        <div class="mb-3 col-6">
+          <label for="tipodoc_doc" class="form-label">Tipo Documento</label>
+          <select id="tipodoc_doc" class="form-select" v-model="tipodoc_doc">
+            <option selected>Selecione</option>
+            <option value="DNI">D.N.I</option>
+            <option value="Carnet">Carnet de Extranjería</option>
+          </select>
+        </div>
+        <div class="mb-3 col-6">
+          <label for="nrodoc_doc" class="form-label">Nro Documento</label>
+          <input
+            type="text"
+            class="form-control"
+            id="nrodoc_doc"
+            v-model="RegistroVacunas.Dosis.Tecnica.NumeroDocumento"
+          />
+        </div>
+        <div class="mb-3 col-6 col-md-4">
+          <label for="firstname_doc" class="form-label">Nombres</label>
+          <input
+            type="text"
+            class="form-control"
+            id="firstname_doc"
+            v-model="RegistroVacunas.Dosis.Tecnica.Nombres"
+          />
+        </div>
+        <div class="mb-3 col-6 col-md-4">
+          <label for="lastname1_doc" class="form-label">Apellido Paterno</label>
+          <input
+            type="text"
+            class="form-control"
+            id="lastname1_doc"
+            v-model="RegistroVacunas.Dosis.Tecnica.ApellidoPaterno"
+          />
+        </div>
+        <div class="mb-3 col-6 col-md-4">
+          <label for="lastname2_doc" class="form-label">Apellido Materno</label>
+          <input
+            type="text"
+            class="form-control"
+            id="lastname2_doc"
+            v-model="RegistroVacunas.Dosis.Tecnica.ApellidoMaterno"
+          />
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <button class="btn btn-primary col-3" @click="addPaciente">
+          Submit
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "RegisterVaccine",
+  props: {
+    title: String
+  },
+  data() {
+    return {
+      // data paciente
+      tipodoc: "",
+      nrodoc: "",
+      firstname: "",
+      lastname1: "",
+      lastname2: "",
+      phone: "",
+      // data vacuna
+      VaccineBrand: "",
+      VaccinationSite: "",
+      datetime: "",
+      dosis: 1,
+      // data doc
+      tipodoc_doc: "",
+      nrodoc_doc: "",
+      firstname_doc: "",
+      lastname1_doc: "",
+      lastname2_doc: "",
+      // desde aca
+      RegistroVacunas: {
+        FechaNacimiento: "",
+        Nombres: "",
+        ApellidoPaterno: "",
+        ApellidoMaterno: "",
+        NumeroDocumento: "",
+        Telefono: "",
+        Dosis: {
+          numeroDosis: 1,
+          LugarVacunacion: "",
+          MarcaVacuna: "",
+          FechaVacunacion: "",
+          Tecnica: {
+            Nombres: "",
+            ApellidoPaterno: "",
+            ApellidoMaterno: "",
+            NumeroDocumento: ""
+          }
+        }
+      }
+    };
+  },
+  methods: {
+    addPaciente: function() {
+      console.log("click p mano");
+      this.getDataDNI(this.nrodoc);
+    },
+    getDataDNI(dni) {
+      console.log(dni);
+      const URLgetDNI = "https://dni.optimizeperu.com/api/prod/persons/";
+      const TOKEN = "k4d2956bd531ab61d44f4fa07304b20e13913815";
+      axios
+        .create({
+          baseURL: URLgetDNI,
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "token " + TOKEN
+          }
+        })
+        .get(dni)
+        .then(res => {
+          console.log(res);
+        });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
